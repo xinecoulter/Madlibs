657b1b59eaa71a6858638a63ab2c59c01b3e8682
Christine Coulter <christinetcoulter@gmail.com>
2013-06-18 22:42:37 -0400

    Add madlibs file

---
+++ madlibs.rb
@@ -0,0 +1,32 @@
+#Asks for nouns
+puts "Give me a noun."
+first_noun = gets.chomp
+
+puts "Give me another noun."
+second_noun = gets.chomp
+
+puts "Give me a third noun."
+third_noun = gets.chomp
+
+#Asks for verbs
+puts "Give me verb."
+first_verb = gets.chomp
+
+puts "Give me another verb."
+second_verb = gets.chomp
+
+puts "Give me a third verb."
+third_verb = gets.chomp
+
+#Asks for adjectives
+puts "Give me an adjective."
+first_adjective = gets.chomp
+
+puts "Give me another adjective."
+second_adjective = gets.chomp
+
+puts "Give me a third adjective."
+third_adjective = gets.chomp
+
+#Shows story
+puts "A #{first_noun} #{first_verb}s into a #{first_adjective} #{second_noun}. The #{first_noun} then #{second_verb}s in a #{second_adjective} fashion. Then, a #{third_noun} #{third_verb}s a #{third_adjective} watermelon."
